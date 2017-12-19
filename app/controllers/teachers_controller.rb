a3d43a4d4a6975e8d78d0bee29bda6af1e7f23c1
nakazawasho <syodobvsyo@gmail.com>
2017-12-19 15:09:11 +0900

    fix teachers_controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,16 +2,4 @@ class TeachersController < ApplicationController
   def index
   end
 
-  def new
-    @teacher = Teacher.new
-  end
-
-  def create
-    Teacher.create(teacher_params)
-  end
-
-  private
-  def teacher_params
-    params.require(:teacher).permit(:name)
-  end
 end
