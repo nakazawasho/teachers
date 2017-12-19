d46e743071ba2d389e135b5e60505eac82c4c833
nakazawasho <syodobvsyo@gmail.com>
2017-12-20 01:38:20 +0900

    fix index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :students, controllers: {
     sessions:      'students/sessions',
     passwords:     'students/passwords',
-    registrations: 'studetns/registrations'
+    registrations: 'students/registrations'
   }
   root "teachers#index"
   resources :teachers
