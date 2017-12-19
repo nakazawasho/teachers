9a0be6cfd271c59adcd626d1b62f7e90df8d2e1f
nakazawasho <syodobvsyo@gmail.com>
2017-12-19 15:04:07 +0900

    image_uploader

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,17 @@
 class TeachersController < ApplicationController
   def index
+  end
 
+  def new
+    @teacher = Teacher.new
+  end
+
+  def create
+    Teacher.create(teacher_params)
+  end
+
+  private
+  def teacher_params
+    params.require(:teacher).permit(:name)
   end
 end
