387fe71c6e3d300843e211da2afb21d2e4842c0c
nakazawasho <syodobvsyo@gmail.com>
2017-12-23 12:58:30 +0900

    fix index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :teachers, controllers: {
-    sessions:      'teachers/sessions',
-    passwords:     'teachers/passwords',
-    registrations: 'teachers/registrations'
-  }
-  devise_for :students, controllers: {
-    sessions:      'students/sessions',
-    passwords:     'students/passwords',
-    registrations: 'students/registrations'
-  }
+  devise_for :teachers
+  devise_for :students
   root "teachers#index"
   resources :teachers
   resources :students
