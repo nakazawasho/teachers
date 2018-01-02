35a4bf503c31fe8b88c01536ba84c1d340295199
nakazawasho <syodobvsyo@gmail.com>
2018-01-02 23:42:50 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   devise_for :students
 
   root "teachers#index"
-  resources :teachers do
+  resources :teachers, without: [:new, :create, :destroy] do
     collection do
       get 'search'
       post 'search' => 'teachers#start_searching'
     end
   end
 
-  resources :students
-  resources :chat_groups do
-    resources :messages
+  resources :students, only: :show
+  resources :chat_groups, only: [:show, :create] do
+    resources :messages, only: :create
   end
 end
