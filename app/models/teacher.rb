fd8f7a5f975c5050c872339f71b0f722cd8cd87d
nakazawasho <syodobvsyo@gmail.com>
2018-01-04 18:27:55 +0900

    fix

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -51,7 +51,7 @@ class Teacher < ApplicationRecord
     teacher.provider = auth.provider
     teacher.uid = auth.uid
     teacher.email = auth.info.email
-    teacher.image = auth.info.image
+    teacher.image = auth.image
     teacher.password = Devise.friendly_token[0,20]
     teacher.save
   end
